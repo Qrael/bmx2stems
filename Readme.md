cf3e6c353aea9c339aee78e569943842845650a9
Zokhoi <20432565+Zokhoi@users.noreply.github.com>
2023-06-10 22:01:16 +0800

    support bpm change

--- Readme.md
+++ Readme.md
@@ -1,9 +1,9 @@
 # bmx2stems: BMS stem/multitrack extractor
 This script renders bms chart formatted songs into a stem pack, with each instrument in their own audio track.
 
-It relies on the samples that comes with the chart being named with their instruments. 
+It relies on the samples that comes with the chart being named with their instruments. Therefore charts like [B.B.K.K.B.K.K.](https://manbow.nothing.sh/event/event.cgi?action=More_def&num=152&event=88) wouldn't work with this script without extensive renaming of the samples.
 
-Currently only songs with a constant bpm is supported. If there is a bpm change in the song, only the first bpm will be considered.
+Currently only songs with or without bpm change but not stops are supported.
 
 Use this script by having .bms file and their supporting audio files in the same directory,
 then providing the .bms file path to the script with:
@@ -14,11 +14,22 @@ node main.js [options] [--] /path/to/chart.bms
 Available options:
 * `-f`, `--format`: output format, `wav` or `ogg`. Default `wav`.
 * `-s`, `--separator`: regular expression for separating instrument name from numbering in the audio file names. Default `"_"`. <br />
+  all that comes before the regexp is regarded as instrument name, and the remaining string is unused, therefore no need to worry about not matching the numbering. <br />
   e.g.  `"_"` for [Chronostasis](https://manbow.nothing.sh/event/event.cgi?action=More_def&num=252&event=110), 
   and `"\s\#|\s?\(|\s[A-G]\#?\d"` for [GOODTEK](https://manbow.nothing.sh/event/event.cgi?action=More_def&num=83&event=104).
-* `-o`, `--outDir`: output directory. Default `stems/` under the directory of the .bms file.
+* `-o`, `--outDir`: output directory. Default `stems/` under the same directory as the .bms file.
 * `--log`: log level, `error`, `warn`, `info`, `verbose`. Default `info`.
 * `-v`: equivalent to `--log verbose`.
+
+## Test suite
+* [立秋 - 竹](https://manbow.nothing.sh/event/event.cgi?action=More_def&num=365&event=133) 
+  * separator `"\_\d\d\d\_|\_\d\."`
+  * bpm changes on channels 03 and 08
+* [LeaF - Aleph-0](https://manbow.nothing.sh/event/event.cgi?action=More_def&num=498&event=110)
+  * separator `"\_\d\d\d\_|\_\d\."`
+  * bpm changes on channels 03 and 08
+  * has stops on channel 09
+  * if-else blocks with randomness 1
 
 ## References
 * [BMS command memo (draft)](https://hitkey.nekokan.dyndns.info/cmds.htm)
--- main.js
+++ main.js
@@ -62,17 +62,47 @@ let bars = sections[1].split(/(?:\r?\n){2,}/g).filter(v=>!!v).map(v=>v.split(/\r
 bars.sort((a,b)=>parseInt(a[0].match(/\#(\d\d\d)[a-zA-Z0-9]{2}:/)[1]) - parseInt(b[0].match(/\#(\d\d\d)[a-zA-Z0-9]{2}/)[1]));
 let data = bars.flat(1);
 
+/** 
+ * Parse random if-endif blocks
+ * Both header and data section may contain them
+ */
+let randomRegex = /\#random \d+(?:(?!\#random)(?:\s|\S))+\#endif/gi;
+let randomnessRegex = /\#random (\d+)/i;
+let ifRegex = /\#if(?:(?!\#if)(?:\s|\S)+\#endif)/gi;
+let parseRandom = v => {
+  return {
+    randomness: parseInt(v.match(randomnessRegex)?.[1]),
+    if: v.match(ifRegex)?.map(k=>{
+      let lines = k.split(/(?:\r?\n)+/g);
+      return {
+        condition: parseInt(lines[0].match(/^\#if (\d+)/)?.[1]),
+        content: lines.filter(k=>!/^\#(end)?if/i.test(k)),
+      }
+    }),
+  }
+}
+
+let allRandoms = src.match(randomRegex)?.map(parseRandom);
+let headerRandoms = sections[0].match(randomRegex)?.map(parseRandom);
+let dataRandoms = sections[1].match(randomRegex)?.map(parseRandom);
+
+if (allRandoms?.length) {
+  winston.info(`There are randomness in this chart. Generated results may not be as expected.`)
+}
+
+
 /**
  * Find bpm, number of bars and bar meters,
  * for calculating the length of the song.
  * 
  * The time signature of the bar will be [meter]/4,
  * thus the length of song will be \sum_{i=0}^{numBars-1}(meter_i*60/bpm).
- * 
- * TODO: consider bpm change, specified on channel 03.
+ * There can also be bpm change during the song,
+ * so the formula above is just figurative.
  */
-let bpmcmd = header.find(v=>v.startsWith("#BPM"))?.split(" ")[1];
-let bpm = bpmcmd ? parseInt(bpmcmd) : 130;
+let bpmCmd = header.find(v=>v.toLowerCase().startsWith("#bpm "))?.split(" ")[1];
+let bpm = bpmCmd ? parseInt(bpmCmd) : 130;
+
 let numBars = parseInt(data[data.length-1].substring(1,4));
 let barMeters = new Array(numBars+3).fill(4);
 for (let i = 0; i < bars.length; i++) {
@@ -80,9 +110,103 @@ for (let i = 0; i < bars.length; i++) {
   let meter = bars[i].find(v=>v.startsWith(`#${bar}02:`))?.split(":")[1];
   if (meter) barMeters[parseInt(bar)] = parseFloat(meter)*4;
 }
+
+/** 
+ * Consider bpm changes. 
+ * 
+ * bpm changes on channel 03 uses 01 to FF for integer bpms between 1 and 255.
+ * bpm changes on channel 08 uses specified #BPMxx for real number bpms.
+ */
+let bpms = header.filter(v=>/\#(ex)?bpm\w\w/i.test(v.toLowerCase()));
+if (bpms.length) {
+  winston.warn(`There are bpm change in this song. Generated results may not be as expected.`)
+}
+let bpmMap = new Map(bpms.map(v=>[v.substring(4,6), parseFloat(v.split(" ")[1])]));
+let bpmSeq = data.filter(v=>/\#\d\d\d0[38]/.test(v)) ?? [];
+let bpmChanges = [];
+
+if (allRandoms?.length) {
+  for (let i = 0; i < allRandoms.length; i++) {
+    if (allRandoms[i].randomness === 1) {
+      /** This random block has only one path, why is it in the block then??? */
+      bpmSeq = bpmSeq.concat(allRandoms[i].if[0].content.filter(v=>/\#\d\d\d0[38]/.test(v)) ?? []);
+    }
+  }
+}
+
+for (let i = 0; i < bpmSeq.length; i++) {
+  let bar = parseInt(bpmSeq[i].substring(1,4));
+  let channel = bpmSeq[i].substring(4,6);
+  let arrange = bpmSeq[i].split(":")[1];
+  if (arrange.length%2) {
+    winston.warn(`Command has odd-lengthed object sequence: \n${bpmSeq[i]}`)
+  }
+  let divide = Math.floor(arrange.length/2);
+  for (let j = 0; j < divide; j++) {
+    let bpmId = arrange.substring(j*2, j*2+2);
+    if (bpmId === "0" || bpmId==="00") continue;
+
+    let newBpm;
+    if (channel==="08") newBpm = bpmMap.get(bpmId);
+    else newBpm = parseInt(bpmId, 16);
+
+    if (newBpm===undefined) {
+      winston.warn(`BPM id ${bpmId} definition not found. Skipping.`);
+      winston.debug(bpmSeq[i])
+      continue;
+    }
+    if (isNaN(newBpm)) {
+      winston.warn(`BPM value ${bpmId} is invalid. Skipping.`)
+      winston.debug(bpmSeq[i])
+      continue;
+    }
+    bpmChanges.push([bar, j/divide, newBpm]);
+  }
+}
+if (!bpmChanges.length || bpmChanges[0][1]!=0 || bpmChanges[0][0]!=0) {
+  bpmChanges.unshift([0,0,bpm]);
+}
+
+bpmChanges.sort((a,b)=>{
+  if (a[0]==b[0]) return a[1]-b[1];
+  else return a[0]-b[0];
+})
+
 /** Song length in number of seconds. */
-let songLength = barMeters.reduce((r, v)=>r+v*60/bpm, 0);
-winston.info(`Song lasts ${songLength} seconds, with bpm ${bpm}.`)
+let songLength = 0;
+if (bpmChanges.length>1) {
+  let bpmChangeIdx = 0;
+  let currentBpm = bpm;
+  let prevBeat = 0;
+  for (let i = 0; i < barMeters.length; i++) {
+    while (true) {
+      if (bpmChanges[bpmChangeIdx]?.[0]==i) {
+        /**
+         * Current bar has bpm change(s)
+         * Add the time up between previous beat and this beat
+         */
+        // winston.debug(bpmChanges[bpmChangeIdx])
+        songLength += barMeters[i]*(bpmChanges[bpmChangeIdx][1]-prevBeat)*60/currentBpm;
+        prevBeat = bpmChanges[bpmChangeIdx][1];
+        currentBpm = bpmChanges[bpmChangeIdx][2];
+        bpmChangeIdx++;
+      } else {
+        /**
+         * Current bar has no (more) bpm change
+         * Just add the (remaining) length of this bar
+         */
+        songLength += barMeters[i]*(1-prevBeat)*60/currentBpm;
+        prevBeat = 0;
+        break;
+      }
+    }
+  }
+} else songLength = barMeters.reduce((r, v)=>r+v*60/bpmChanges[0][2], 0);
+
+winston.info(`Song lasts ${songLength} seconds, with bpm ${bpmChanges[0][2]}.`)
+if (bpmChanges.length>1) {
+  winston.info(`There are ${bpmChanges.length-1} bpm changes, ranging between ${Math.min(...bpmChanges.map(v=>v[2]), bpm)} and ${Math.max(...bpmChanges.map(v=>v[2]), bpm)}.`)
+}
 
 /**
  * All the audio files, mapped to the audio file ID.
@@ -132,6 +256,8 @@ let audioFileMap = new Map(header.filter(v=>v.toLowerCase().startsWith("#wav")||
     return channel == 1 || Math.floor(channel/10)>=1 && Math.floor(channel/10)<=6 && channel%10!=0 && channel%10!=7;
   });
 
+  audioSeq.sort((a,b)=>parseInt(a.match(/\#(\d\d\d)[a-zA-Z0-9]{2}:/)[1]) - parseInt(b.match(/\#(\d\d\d)[a-zA-Z0-9]{2}/)[1]));
+
   /** Load audio files */
   audioFileMap.forEach(v=>{
     v.fileBuf = fs.readFileSync(path.resolve(filedir, v.file));
@@ -141,45 +267,136 @@ let audioFileMap = new Map(header.filter(v=>v.toLowerCase().startsWith("#wav")||
   let currentSample = 0;
   /** Previous bar number */
   let prevbar = 0;
+  /** Current bar beat's starting timestamp, in samples */
+  let barBeatSample = 0;
+  /** Current bar's bpm change list index */
+  let bpmChangeIdx = 0;
+  let prevBarBpm = bpm;
+  let currentBpm = bpm;
+  let prevBeat = 0;
   for (let i = 0; i < audioSeq.length; i++) {
+    /** Basic info for this audio sequence */
     let bar = parseInt(audioSeq[i].split(":")[0].substring(1, 4));
+    let arrange = audioSeq[i].split(":")[1];
+    if (arrange.length%2) {
+      winston.warn(`Command has odd-lengthed object sequence: \n${audioSeq[i]}`);
+    }
+    let divide = Math.floor(arrange.length/2);
+    // winston.debug(`meter: ${barMeters[bar]}`)
+
+
+    /** Index and stuff */
     if (prevbar!=bar) {
       winston.verbose(`Processing bar ${bar}`);
-      for (let j = prevbar; j < bar; j++) {
-        currentSample += Math.floor(barMeters[j]*60*sampleRate/bpm);
+      // winston.debug(`Prev BPM ${prevBarBpm}, BPM ${currentBpm}`)
+      // winston.debug(barBeatSample)
+      currentSample += barBeatSample;
+      /** If bars are skipped, calculate them */
+      for (let j = prevbar+1; j < bar; j++) {
+        currentSample += Math.floor(barMeters[j]*60*sampleRate/currentBpm);
       }
       prevbar = bar;
+      let prevChanges = bpmChanges.filter(v=>v[0]<bar);
+      if (prevChanges.length) {
+        prevBarBpm = prevChanges[prevChanges.length-1][2];
+      }
+    } else {
+      /** 
+       * Reset bpm back to previous bar
+       * because we are handling the same bar,
+       * which might have been bpm-changed later in the bar
+       */
+      currentBpm = prevBarBpm;
     }
-    let arrange = audioSeq[i].split(":")[1];
-    let divide = arrange.length/2;
-    // console.log(`meter: ${barMeters[bar]}`)
-    let beatLength = Math.floor(barMeters[bar]*60*sampleRate/bpm/divide);
-    // console.log(`Beat length: ${beatLength}`)
+    bpmChangeIdx = 0;
+    barBeatSample = 0;
+    prevBeat = 0;
+    let barBpmChange = bpmChanges.filter(v=>v[0]==bar);
+
+
+    // winston.debug(`Base BPM: ${bpm}, current BPM: ${currentBpm}`)
+    // winston.debug(`Beat length: ${barMeters[bar]*60*sampleRate/bpm/divide}`)
     for (let j = 0; j < divide; j++) {
       let audioId = arrange.substring(j*2, j*2+2);
       if (audioId === "0" || audioId === "00") continue;
-      // console.log(audioId);
-
-
-      /** Process audio sample */
+      // winston.debug(audioId);
       let audio = audioFileMap.get(audioId);
       if (audio === undefined) {
         winston.warn(`Audio file id ${audioId} definition not found. Skipping.`);
         continue;
       }
+
+      if ((barBpmChange[bpmChangeIdx]!==undefined ? barBpmChange[bpmChangeIdx]?.[1]*barMeters[bar] : j+1) <= j) {
+        /**
+         * There is a bpm change on or before this beat
+         * Add the time up between previous beat and this beat
+         */
+        // winston.debug(currentBpm)
+        // winston.debug(barBpmChange[bpmChangeIdx])
+        barBeatSample += barMeters[bar]*(barBpmChange[bpmChangeIdx][1]-prevBeat)*60*sampleRate/currentBpm;
+        barBeatSample += barMeters[bar]*(j/divide-barBpmChange[bpmChangeIdx][1])*60*sampleRate/barBpmChange[bpmChangeIdx][2];
+        // if (!barBeatSample) winston.debug(`${barMeters[bar]}, ${j/divide}, ${prevBeat}, ${currentBpm}`)
+        currentBpm = barBpmChange[bpmChangeIdx][2];
+        // winston.debug(currentBpm)
+        bpmChangeIdx++;
+      } else {
+        /**
+         * Current bar has no (more) bpm change
+         */
+        barBeatSample += barMeters[bar]*(j/divide-prevBeat)*60*sampleRate/currentBpm;
+        // if (!barBeatSample) winston.debug(`${barMeters[bar]}, ${j/divide}, ${prevBeat}, ${currentBpm}`)
+      }
+      prevBeat = j/divide;
+      // winston.debug(barMeters[bar])
+      // winston.debug(j/divide-prevBeat)
+
+      /** Process audio sample */
+      // winston.debug(`Bar ${bar} beat ${j/divide*barMeters[bar]} (${barBeatSample}) for sound ${audioId} (${audio.file})`)
       let decodedAudio = await decodeAudio(audio.fileBuf);
       let daLeft = decodedAudio.getChannelData(0);
       let daRight = decodedAudio.getChannelData(1);
       let stemTrack = stemSplits.get(audio.inst);
       let left = stemTrack.getChannelData(0);
       let right = stemTrack.getChannelData(1);
-      // console.log(`Audio sample lasts for ${decodedAudio.length} samples`);
-      // console.log(`At beat ${currentSample + beatLength*j}`);
-      for (let k = 0; k < decodedAudio.length; k++) {
-        left[currentSample + beatLength*j + k]+=daLeft[k];
-        right[currentSample + beatLength*j + k]+=daRight[k];
+      // winston.debug(`Track ${audio.inst}: ${stemTrack.length}`)
+
+      /** 
+       * Allocate new AudioBuffer if the sample length+position
+       * is longer than what the current one can hold
+       */
+      if (currentSample + Math.floor(barBeatSample) + decodedAudio.length > stemTrack.length) {
+        let newTrack = new AudioBuffer({
+          length: currentSample + Math.floor(barBeatSample) + decodedAudio.length,
+          sampleRate,
+          numberOfChannels: 2,
+        })
+        newTrack.copyToChannel(left, 0, 0);
+        newTrack.copyToChannel(right, 1, 0);
+        stemTrack = newTrack;
+        stemSplits.set(audio.inst, newTrack);
+        left = newTrack.getChannelData(0);
+        right = newTrack.getChannelData(1);
       }
+
+      // winston.debug(`Audio sample lasts for ${decodedAudio.length} samples`);
+      // winston.debug(`At beat ${Math.floor(currentSample + barBeatSample)}`);
+
+      /** Copy the sample to inst track */
+      for (let k = 0; k < decodedAudio.length; k++) {
+        left[Math.floor(currentSample + barBeatSample) + k]+=daLeft[k];
+        right[Math.floor(currentSample + barBeatSample) + k]+=daRight[k];
+      }
+
+      // winston.debug(`Now bpm: ${currentBpm}`)
     }
+
+    /** Add the last beats to complete the bar for barBeatSample */
+    for (let j = bpmChangeIdx; j < barBpmChange.length; j++) {
+      barBeatSample += barMeters[bar]*(barBpmChange[j][1]-prevBeat)*60*sampleRate/currentBpm;
+      prevBeat = barBpmChange[j][1];
+      currentBpm = barBpmChange[j][2];
+    }
+    barBeatSample += barMeters[bar]*(1-prevBeat)*60*sampleRate/currentBpm;
   }
 
   winston.info(`Saving ${instruments.length} stem tracks to ${outDir}...`);
